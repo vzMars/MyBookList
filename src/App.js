78c3e373af175a4f3a777d68fa68265157014779
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-17 16:28:52 -0400

    created starter public and private routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// components
+import RequireAuth from './components/RequireAuth';
+import RequireGuest from './components/RequireGuest';
+
+// pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
+// layouts
+import Layout from './layouts/Layout';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>MyBookList</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route element={<RequireGuest />}>
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+          </Route>
+
+          <Route element={<RequireAuth />}>
+            <Route path='profile' element={<Profile />} />
+          </Route>
+
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
