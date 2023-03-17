58dd7fb58c467392ee42aae99cfbcecdd52146a2
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-17 16:17:26 -0400

    components that checks for user/guest before rendering routes and navigating to login/index if not allowed

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+
+const RequireAuth = () => {
+  const { user } = useAuthContext();
+  return user ? <Outlet /> : <Navigate to='/login' />;
+};
+
+export default RequireAuth;
