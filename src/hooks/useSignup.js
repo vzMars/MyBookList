a103151f39e0d07b0538d717a6097f8df5c448c7
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-19 14:06:51 -0400

    hook for signing up a user (updates the authcontext with the user that signed up) and with error handling

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const signup = async (email, userName, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch('http://localhost:5000/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ email, userName, password }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      dispatch({ type: 'LOGIN', payload: json.user });
+      setIsLoading(false);
+    }
+  };
+
+  return { signup, isLoading, error };
+};
+
+export default useSignup;
