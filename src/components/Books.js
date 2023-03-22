7de933849765cd451626d05a63acef6407880e2e
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-22 18:24:29 -0400

    render book covers instead of titles and when user clicks they are linked to that book's details page

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,10 +4,25 @@ const Books = ({ books, loading }) => {
   }
 
   return (
-    <ul className='mb-5'>
+    <ul className='mx-5 my-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 md:mx-0 lg:grid-cols-5 xl:gap-x-8'>
       {books.map((book) => (
-        <li className='text-white' key={book.key}>
-          {book.title}
+        <li
+          key={book.id}
+          className='w-full overflow-hidden rounded-md bg-neutral-800 lg:h-80 border border-neutral-500 hover:border-neutral-200 hover:border-2'
+        >
+          <a href={`/books/${book.id}`}>
+            {book.volumeInfo.imageLinks ? (
+              <img
+                className='h-full w-full object-cover object-center'
+                src={book.volumeInfo.imageLinks.thumbnail}
+                alt={book.volumeInfo.title}
+              />
+            ) : (
+              <div className='h-full w-full object-cover object-center text-white flex justify-center items-center'>
+                <div className='text-center mx-5'>No image available</div>
+              </div>
+            )}
+          </a>
         </li>
       ))}
     </ul>
