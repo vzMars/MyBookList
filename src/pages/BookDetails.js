55d1a755ab891372b001e734d16ed93c762ccb6a
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-22 20:50:59 -0400

    added default loading spinner for when book details page first loads

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const BookDetails = () => {
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
+
+  // useEffect(() => {
+  //   const response = await fetch(
+  //     `http://localhost:5000/api/books/search/${query}`,
+  //     {
+  //       method: 'GET',
+  //       credentials: 'include',
+  //     }
+  //   );
+  // }, [])
+
   return (
-    <main>
-      <h1>BookDetails {id}</h1>
+    <main className='max-w-6xl mx-auto mt-10 w-full'>
+      <section className='px-3 md:px-10 flex flex-col'>
+        {loading ? (
+          <span className='loader self-center mt-5'></span>
+        ) : (
+          <h1>BookDetails {id}</h1>
+        )}
+      </section>
     </main>
   );
 };
