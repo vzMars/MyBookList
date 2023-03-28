5f59321545eb2102a83d78c9d5b54ac22682b7d8
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-27 21:52:22 -0400

    default image for books that have no cover, used on books that dont have coverse in the search results

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,20 @@
+import cover from '../images/cover.png';
+import { Link } from 'react-router-dom';
+
 const Book = ({ book }) => {
   return (
     <li className='w-full overflow-hidden rounded-md bg-neutral-800 lg:h-80 border border-neutral-500 hover:border-neutral-200 hover:border-2'>
-      <a href={`/books/${book.id}`}>
-        {book.volumeInfo.imageLinks ? (
-          <img
-            className='h-full w-full object-cover object-center'
-            src={book.volumeInfo.imageLinks.thumbnail}
-            alt={book.volumeInfo.title}
-          />
-        ) : (
-          <div className='h-full w-full object-cover object-center text-white flex justify-center items-center'>
-            <div className='text-center mx-5'>No image available</div>
-          </div>
-        )}
-      </a>
+      <Link to={`/books/${book.id}`}>
+        <img
+          className='h-full w-full object-cover object-center'
+          src={
+            !book.volumeInfo.imageLinks
+              ? cover
+              : book.volumeInfo.imageLinks.thumbnail
+          }
+          alt={book.volumeInfo.title}
+        />
+      </Link>
     </li>
   );
 };
