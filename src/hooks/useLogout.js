e854f737f1b5179de7de33731979e33dc06163e4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-01 19:07:03 -0400

    when user logout clear the global book state

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from './useAuthContext';
+import { useBookContext } from './useBookContext';
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: bookDispatch } = useBookContext();
 
   const logout = async () => {
     const response = await fetch('http://localhost:5000/api/auth/logout', {
@@ -11,6 +13,7 @@ export const useLogout = () => {
 
     if (response.ok) {
       dispatch({ type: 'LOGOUT' });
+      bookDispatch({ type: 'SET_BOOKS', payload: null });
     }
   };
 
