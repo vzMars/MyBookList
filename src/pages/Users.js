1273ab53e054ed538145f4fd0a31b3f4daa5a290
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-01 16:54:51 -0400

    page that renders a ul contains links to all the users of the app

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useBookContext } from '../hooks/useBookContext';
+import UserCard from '../components/UserCard';
 
 const Users = () => {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { books } = useBookContext();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      setLoading(true);
+      const response = await fetch('http://localhost:5000/api/auth/users', {
+        method: 'GET',
+        credentials: 'include',
+      });
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        setLoading(false);
+      }
+
+      if (response.ok) {
+        setUsers(json);
+        setLoading(false);
+      }
+    };
+
+    getUsers();
+  }, []);
+
   return (
-    <main className='max-w-6xl mx-auto mt-10 w-full'>
+    <main className='max-w-6xl mx-auto my-10 w-full'>
       <section className='px-3 md:px-10 flex flex-col'>
-        <h1 className='text-5xl font-bold mb-5 text-red-600'>Users</h1>
+        {loading ? (
+          <span className='loader self-center mt-5'></span>
+        ) : (
+          <div>
+            <h1 className='text-5xl font-bold mb-5 text-red-600'>Users</h1>
+            <ul className='grid grid-cols-1 my-5 gap-5 md:grid-cols-3 md:space-y-0 '>
+              {users.map((user) => (
+                <UserCard
+                  key={user._id}
+                  user={user}
+                  books={books.filter((book) => book.user._id === user._id)}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
       </section>
     </main>
   );
